2b71bb0d77d99b8b4664ceee53c616bedd9cc09a
Gulam Jilani <125073252+gulamjilani25@users.noreply.github.com>
2023-03-15 18:10:50 +0530

    Add files via upload

---
+++ src/DEmo/ClassTest.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class DemoClass extends React.Component {
+
+
+    render() {
+        return (
+            <div>
+                <h1>Hello class</h1>
+            </div>
+        )
+    }
+}
+export default DemoClass;
---
+++ src/DEmo/Test.js
@@ -0,0 +1,13 @@
+
+function Demo() {
+
+    return (
+        // /<h1>VVVV</h1>
+        <div>
+            vvvvv
+        </div>
+
+    )
+};
+
+export default Demo;
